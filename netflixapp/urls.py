8a787091c88cdd026887c23e413ae6ce5d4c3531
kema-ray <rachelkemuma99@gmail.com>
2023-01-26 13:09:02 +0300

    create views for the app

--- netflixapp/urls.py
+++ netflixapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import *
 
 app_name = 'netflixapp'
 
 urlpatterns = [
-    
+    path('', Home.as_view()),
 ]
--- netflixapp/views.py
+++ netflixapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 from django.views import View
 
-
+class Home(View):
+    def get (self, request,*args, **kwargs):
+        return render(request, 'index.html')
